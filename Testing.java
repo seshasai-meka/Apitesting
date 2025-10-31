79d04c20c20eecaaef76c44841ab35f2f568d677
seshasai-meka <60053430+seshasai-meka@users.noreply.github.com>
2025-10-31 12:44:37 -0400

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,17 @@
+
+
+		
+		
+		
+
+	
+	
+package com.module1;
+
+public class Testing {
+    public static void main(String[] args) {
+        // Print Java version to confirm
+        System.out.println("Hello, Java!");
+        System.out.println("Running Java version: " + System.getProperty("java.version"));
+    }
+}
