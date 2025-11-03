7f8874543b9ff5dd879e6e8ed9dea86d7fc26d73
seshasai-meka <60053430+seshasai-meka@users.noreply.github.com>
2025-11-03 11:00:55 -0500

    Update Cucumber features path in CucumberRunnerAPI

--- APIProject/src/test/java/com/fdmgroup/stepdefinitions/CucumberRunnerAPI.java
+++ APIProject/src/test/java/com/fdmgroup/stepdefinitions/CucumberRunnerAPI.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @CucumberOptions(
 
-		features="src/test/resources",
+		features="features = "src/test/java/com/fdmgroup/resources",
 
        glue= {"com.fdmgroup.stepdefinitions"},
 
